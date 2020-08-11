a52190128df183939a5a547b64d297ba7a74e677
Samuel Marcellinus <samuelmarcell21@gmail.com>
2020-08-11 22:40:31 +0700

    add models in topic

--- topic/models.py
+++ topic/models.py
@@ -1,3 +1,12 @@
+from django.db import connections
 from django.db import models
 
 # Create your models here.
+class Topics(models.Model):
+    id_topic = models.CharField(max_length=20)
+    topic_name = models.CharField(max_length=200)
+    total_author = models.IntegerField()
+    total_cite = models.IntegerField()
+    total_publication = models.IntegerField()
+    class Meta:
+        db_table = "topic"
