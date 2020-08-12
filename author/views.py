44676e39d03ca65a86bc926d4747f7bf9ee6dbc3
Samuel Marcellinus <samuelmarcell21@gmail.com>
2020-08-12 14:44:47 +0700

    pagination dan contoh di author

--- author/views.py
+++ author/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from author.models import Authors
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def showauthor(request):
-    result = Authors.objects.all().values('name','nidn','h_index','i10_index')
-    # print(result)
-    return render(request, 'author/author.html', {'author':result})
+    result = Authors.objects.all().values('name','nidn','h_index','i10_index')[:100]
+    print(result)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(result, 20)
+
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
+    return render(request, 'author/author.html', {'users': users})
